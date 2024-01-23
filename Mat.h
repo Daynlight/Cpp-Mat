c6e99beab473d550fdd1003d4b591e0ee401b133
kokomichrzan <kokomichrzanuwu@gmail.com>
2024-01-23 20:41:08 +0100

    Operators, Distance

--- Mat.h
+++ Mat.h
@@ -1,5 +1,15 @@
 #pragma once
 #include <string>
+#include <math.h>
+
+//[TODO]
+// Distance from origin
+// Compare >=, >, <=, <
+// Matrixes
+// Oprators for Matrixes
+// VEC * Matrix
+
+
 
 namespace Mat {
 	template<typename T>
@@ -11,8 +21,76 @@ namespace Mat {
 		Vec2(T a): x(a),y(a) {};
 		Vec2(T x, T y) : x(x), y(y) {};
 
+		bool operator ==(Vec2 Vec) {
+			return (x == Vec.x && y == Vec.y);
+		}
+		bool operator !=(Vec2 Vec) {
+			return !(this->operator==(Vec));
+		}
+		bool operator >(Vec2 Vec) {
+			return (Distance() > Vec.Distance());
+		}
+		bool operator >=(Vec2 Vec) {
+			return (Distance() >= Vec.Distance());
+		}
+		bool operator <(Vec2 Vec) {
+			return (Distance() < Vec.Distance());
+		}
+		bool operator <=(Vec2 Vec) {
+			return (Distance() <= Vec.Distance());
+		}
+		void operator +=(Vec2 Vec) {
+			x = x + Vec.x;
+			y = y + Vec.y;
+		}
+		void operator -=(Vec2 Vec) {
+			x = x - Vec.x;
+			y = y - Vec.y;
+		}
+		void operator *=(Vec2 Vec) {
+			x = x * Vec.x;
+			y = y * Vec.y;
+		}
+		void operator /=(Vec2 Vec) {
+			x = x / Vec.x;
+			y = y / Vec.y;
+		}
+		void operator %=(Vec2 Vec) {
+			x = x % Vec.x;
+			y = y % Vec.y;
+		}
+		Vec2 operator +(Vec2 Vec) {
+			T xR = x + Vec.x;
+			T yR = y + Vec.y;
+			return Vec2(xR, yR);
+		}
+		Vec2 operator -(Vec2 Vec) {
+			T xR = x - Vec.x;
+			T yR = y - Vec.y;
+			return Vec2(xR, yR);
+		}
+		Vec2 operator *(Vec2 Vec) {
+			T xR = x * Vec.x;
+			T yR = y * Vec.y;
+			return Vec2(xR, yR);
+		}
+		Vec2 operator /(Vec2 Vec) {
+			T xR = x / Vec.x;
+			T yR = y / Vec.y;
+			return Vec2(xR, yR);
+		}
+		Vec2 operator %(Vec2 Vec) {
+			T xR = x % Vec.x;
+			T yR = y % Vec.y;
+			return Vec2(xR, yR);
+		}
+
+		double Distance(Vec2 Vec = 0) {
+			return sqrt(pow(x - Vec.x, 2) + pow(y - Vec.y, 2));
+		}
+		
 		// Vec to String
-		const std::string const VTS() {
+		const std::string const str() {
 			return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
 		}
 	};
@@ -28,8 +106,86 @@ namespace Mat {
 		Vec3(T x, T y) : x(x), y(y) {};
 		Vec3(T x, T y, T z) : x(x), y(y), z(z) {};
 
+		bool operator ==(Vec3 Vec) {
+			return (x == Vec.x && y == Vec.y && z == Vec.z);
+		}
+		bool operator !=(Vec3 Vec) {
+			return !(this->operator==(Vec));
+		}
+		bool operator >(Vec3 Vec) {
+			return (Distance() > Vec.Distance());
+		}
+		bool operator >=(Vec3 Vec) {
+			return (Distance() >= Vec.Distance());
+		}
+		bool operator <(Vec3 Vec) {
+			return (Distance() < Vec.Distance());
+		}
+		bool operator <=(Vec3 Vec) {
+			return (Distance() <= Vec.Distance());
+		}
+		void operator +=(Vec3 Vec) {
+			x = x + Vec.x;
+			y = y + Vec.y;
+			z = z + Vec.z;
+		}
+		void operator -=(Vec3 Vec) {
+			x = x - Vec.x;
+			y = y - Vec.y;
+			z = z - Vec.z;
+		}
+		void operator *=(Vec3 Vec) {
+			x = x * Vec.x;
+			y = y * Vec.y;
+			z = z * Vec.z;
+		}
+		void operator /=(Vec3 Vec) {
+			x = x / Vec.x;
+			y = y / Vec.y;
+			z = z / Vec.z;
+		}
+		void operator %=(Vec3 Vec) {
+			x = x % Vec.x;
+			y = y % Vec.y;
+			z = z % Vec.z;
+		}
+		Vec3 operator +(Vec3 Vec) {
+			T xR = x + Vec.x;
+			T yR = y + Vec.y;
+			T zR = z + Vec.z;
+			return Vec3(xR, yR, zR);
+		}
+		Vec3 operator -(Vec3 Vec) {
+			T xR = x - Vec.x;
+			T yR = y - Vec.y;
+			T zR = z - Vec.z;
+			return Vec3(xR, yR, zR);
+		}
+		Vec3 operator *(Vec3 Vec) {
+			T xR = x * Vec.x;
+			T yR = y * Vec.y;
+			T zR = z * Vec.z;
+			return Vec3(xR, yR, zR);
+		}
+		Vec3 operator /(Vec3 Vec) {
+			T xR = x / Vec.x;
+			T yR = y / Vec.y;
+			T zR = z / Vec.z;
+			return Vec3(xR, yR, zR);
+		}
+		Vec3 operator %(Vec3 Vec) {
+			T xR = x % Vec.x;
+			T yR = y % Vec.y;
+			T zR = z % Vec.z;
+			return Vec3(xR, yR, zR);
+		}
+
+		double Distance(Vec3 Vec = 0) {
+			return sqrt(pow(x - Vec.x, 2) + pow(y - Vec.y, 2) + pow(z - Vec.z, 2));
+		}
+
 		// Vec to String
-		const std::string const VTS() {
+		const std::string const str() {
 			return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ")";
 		}
 	};
@@ -47,8 +203,96 @@ namespace Mat {
 		Vec4(T x, T y, T z) : x(x), y(y), z(z) {};
 		Vec4(T x, T y, T z, T h) : x(x), y(y), z(z), h(h) {};
 
+		bool operator ==(Vec4 Vec) {
+			return (x == Vec.x && y == Vec.y && z == Vec.z && h == Vec.h);
+		}
+		bool operator !=(Vec4 Vec) {
+			return !(this->operator==(Vec));
+		}
+		bool operator >(Vec4 Vec) {
+			return (Distance() > Vec.Distance());
+		}
+		bool operator >=(Vec4 Vec) {
+			return (Distance() >= Vec.Distance());
+		}
+		bool operator <(Vec4 Vec) {
+			return (Distance() < Vec.Distance());
+		}
+		bool operator <=(Vec4 Vec) {
+			return (Distance() <= Vec.Distance());
+		}
+		void operator +=(Vec4 Vec) {
+			x = x + Vec.x;
+			y = y + Vec.y;
+			z = z + Vec.z;
+			h = h + Vec.h;
+		}
+		void operator -=(Vec4 Vec) {
+			x = x - Vec.x;
+			y = y - Vec.y;
+			z = z - Vec.z;
+			h = h - Vec.h;
+		}
+		void operator *=(Vec4 Vec) {
+			x = x * Vec.x;
+			y = y * Vec.y;
+			z = z * Vec.z;
+			h = h * Vec.h;
+		}
+		void operator /=(Vec4 Vec) {
+			x = x / Vec.x;
+			y = y / Vec.y;
+			z = z / Vec.z;
+			h = h / Vec.h;
+		}
+		void operator %=(Vec4 Vec) {
+			x = x % Vec.x;
+			y = y % Vec.y;
+			z = z % Vec.z;
+			h = h % Vec.h;
+		}
+		Vec4 operator +(Vec4 Vec) {
+			T xR = x + Vec.x;
+			T yR = y + Vec.y;
+			T zR = z + Vec.z;
+			T hR = h + Vec.h;
+			return Vec4(xR, yR, zR, hR);
+		}
+		Vec4 operator -(Vec4 Vec) {
+			T xR = x - Vec.x;
+			T yR = y - Vec.y;
+			T zR = z - Vec.z;
+			T hR = h - Vec.h;
+			return Vec4(xR, yR, zR, hR);
+		}
+		Vec4 operator *(Vec4 Vec) {
+			T xR = x * Vec.x;
+			T yR = y * Vec.y;
+			T zR = z * Vec.z;
+			T hR = h * Vec.h;
+			return Vec4(xR, yR, zR, hR);
+		}
+		Vec4 operator /(Vec4 Vec) {
+			T xR = x / Vec.x;
+			T yR = y / Vec.y;
+			T zR = z / Vec.z;
+			T hR = h / Vec.h;
+			return Vec4(xR, yR, zR, hR);
+		}
+		Vec4 operator %(Vec4 Vec) {
+			T xR = x % Vec.x;
+			T yR = y % Vec.y;
+			T zR = z % Vec.z;
+			T hR = h % Vec.h;
+			return Vec4(xR, yR, zR, hR);
+		}
+
+		double Distance(Vec4 Vec = 0) {
+			return sqrt(pow(x - Vec.x, 2) + pow(y - Vec.y, 2) + pow(z - Vec.z, 2) + pow(h - Vec.h, 2));
+		}
+
 		// Vec to String
-		const std::string const VTS() {
+		const std::string const str() {
 			return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + 
 						 std::to_string(z) + ", " + std::to_string(h) + ")";
 		}
--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,333 @@
-#include <iostream>
-#include "Mat.h"
+#include "Test.h"
 
 int main() {
-	Mat::Vec4<int> A = Mat::Vec4<int>();
-	std::cout << A.VTS();
+	WorkingTest TestProgram = WorkingTest();
+	TestProgram.ShowAll();
+
 	return 0;
+}
+
+void WorkingTest::ShowAll()
+{
+	ShowCreateVec();
+	ShowDistance();
+	ShowAllOperators();
+}
+
+void WorkingTest::ShowAllOperators()
+{
+	ShowCompare();
+	ShowOperatorWithAnotherVec();
+	ShowOperatorWithAnotherNumber();
+}
+
+void WorkingTest::ShowOperatorWithAnotherVec()
+{
+	std::cout << "\n----------------- Operators With Vectors ----------------\n";
+	ShowOperatorAddWithAnotherVec();
+	ShowOperatorMinusWithAnotherVec();
+	ShowOperatorTimesWithAnotherVec();
+	ShowOperatorDevideWithAnotherVec();
+	ShowOperatorModuloWithAnotherVec();
+	ShowOperatorAddEquelWithAnotherVec();
+	ShowOperatorMinusEquelWithAnotherVec();
+	ShowOperatorTimesEquelWithAnotherVec();
+	ShowOperatorDevideEquelWithAnotherVec();
+	ShowOperatorModuloEquelWithAnotherVec();
+}
+
+void WorkingTest::ShowOperatorWithAnotherNumber()
+{
+	std::cout << "\n----------------- Operators With Numbers ----------------\n";
+	ShowOperatorAddWithAnotherNumber();
+	ShowOperatorMinusWithAnotherNumber();
+	ShowOperatorTimesWithAnotherNumber();
+	ShowOperatorDevideWithAnotherNumber();
+	ShowOperatorModuloWithAnotherNumber();
+	ShowOperatorAddEquelWithAnotherNumber();
+	ShowOperatorMinusEquelWithAnotherNumber();
+	ShowOperatorTimesEquelWithAnotherNumber();
+	ShowOperatorDevideEquelWithAnotherNumber();
+	ShowOperatorModuloEquelWithAnotherNumber();
+}
+
+void WorkingTest::ShowCreateVec() {
+	std::cout << "\n----------------- Create ----------------\n";
+	std::cout << Vec2.str() << "\n";
+	std::cout << Vec3.str() << "\n";
+	std::cout << Vec4.str() << "\n";
+}
+
+void WorkingTest::ShowDistance()
+{
+	std::cout << "\n----------------- Distance ----------------\n";
+	std::cout << Vec2.str() + ".Distance(); = " << Vec2.Distance() << "\n";
+	std::cout << Vec3.str() + ".Distance(); = " << Vec3.Distance() << "\n";
+	std::cout << Vec4.str() + ".Distance(); = " << Vec4.Distance() << "\n";
+	std::cout << Vec2.str() + ".Distance" + Vec2B.str() + "; = " << Vec2.Distance(Vec2B) << "\n";
+	std::cout << Vec3.str() + ".Distance" + Vec3B.str() + "; = " << Vec3.Distance(Vec3B) << "\n";
+	std::cout << Vec4.str() + ".Distance" + Vec4B.str() + "; = " << Vec4.Distance(Vec4B) << "\n";
+}
+
+void WorkingTest::ShowOperatorAddWithAnotherVec() {
+	std::cout << "\n----------------------------- Operator Add With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " + " << Vec2B.str() << " = " + (Vec2 + Vec2B).str() + "\n";
+	std::cout << Vec3.str() << " + " << Vec3B.str() << " = " + (Vec3 + Vec3B).str() + "\n";
+	std::cout << Vec4.str() << " + " << Vec4B.str() << " = " + (Vec4 + Vec4B).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorMinusWithAnotherVec() {
+	std::cout << "\n----------------------------- Operator Minus With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " - " << Vec2B.str() << " = " + (Vec2 - Vec2B).str() + "\n";
+	std::cout << Vec3.str() << " - " << Vec3B.str() << " = " + (Vec3 - Vec3B).str() + "\n";
+	std::cout << Vec4.str() << " - " << Vec4B.str() << " = " + (Vec4 - Vec4B).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorTimesWithAnotherVec()
+{
+	std::cout << "\n----------------------------- Operator Times With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " * " << Vec2B.str() << " = " + (Vec2 * Vec2B).str() + "\n";
+	std::cout << Vec3.str() << " * " << Vec3B.str() << " = " + (Vec3 * Vec3B).str() + "\n";
+	std::cout << Vec4.str() << " * " << Vec4B.str() << " = " + (Vec4 * Vec4B).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorDevideWithAnotherVec()
+{
+	std::cout << "\n----------------------------- Operator Devide With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " / " << Vec2B.str() << " = " + (Vec2 / Vec2B).str() + "\n";
+	std::cout << Vec3.str() << " / " << Vec3B.str() << " = " + (Vec3 / Vec3B).str() + "\n";
+	std::cout << Vec4.str() << " / " << Vec4B.str() << " = " + (Vec4 / Vec4B).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorModuloWithAnotherVec()
+{
+	std::cout << "\n----------------------------- Operator Modulo With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " % " << Vec2B.str() << " = " + (Vec2 % Vec2B).str() + "\n";
+	std::cout << Vec3.str() << " % " << Vec3B.str() << " = " + (Vec3 % Vec3B).str() + "\n";
+	std::cout << Vec4.str() << " % " << Vec4B.str() << " = " + (Vec4 % Vec4B).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorAddEquelWithAnotherVec()
+{
+	std::cout << "\n----------------------------- Operator Add Equel With Another Vec ----------------------------\n";
+	std::cout << Vec2.str() << " += " << Vec2B.str() << " = ";
+	Vec2 += Vec2B;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() << " += " << Vec3B.str() << " = ";
+	Vec3 += Vec3B;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() << " += " << Vec4B.str() << " = ";
+	Vec4 += Vec4B;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorMinusEquelWithAnotherVec()
+{
+	std::cout << "\n-------------------- Operator Minus Equel With Another Vec --------------------\n";
+	std::cout << Vec2.str() << " -= " << Vec2B.str() << " = ";
+	Vec2 -= Vec2B;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() << " -= " << Vec3B.str() << " = ";
+	Vec3 -= Vec3B;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() << " -= " << Vec4B.str() << " = ";
+	Vec4 -= Vec4B;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorTimesEquelWithAnotherVec()
+{
+	std::cout << "\n-------------------- Operator Times Equel With Another Vec --------------------\n";
+	std::cout << Vec2.str() << " *= " << Vec2B.str() << " = ";
+	Vec2 *= Vec2B;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() << " *= " << Vec3B.str() << " = ";
+	Vec3 *= Vec3B;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() << " *= " << Vec4B.str() << " = ";
+	Vec4 *= Vec4B;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorDevideEquelWithAnotherVec()
+{
+	std::cout << "\n-------------------- Operator Devide Equel With Another Vec --------------------\n";
+	std::cout << Vec2.str() << " /= " << Vec2B.str() << " = ";
+	Vec2 /= Vec2B;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() << " /= " << Vec3B.str() << " = ";
+	Vec3 /= Vec3B;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() << " /= " << Vec4B.str() << " = ";
+	Vec4 /= Vec4B;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorModuloEquelWithAnotherVec()
+{
+	std::cout << "\n-------------------- Operator Modulo Equel With Another Vec --------------------\n";
+	std::cout << Vec2.str() << " %= " << Vec2B.str() << " = ";
+	Vec2 %= Vec2B;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() << " %= " << Vec3B.str() << " = ";
+	Vec3 %= Vec3B;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() << " %= " << Vec4B.str() << " = ";
+	Vec4 %= Vec4B;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorAddWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Add With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " + " + std::to_string(Num) + " = " + (Vec2 + Num).str() + "\n";
+	std::cout << Vec3.str() + " + " + std::to_string(Num) + " = " + (Vec3 + Num).str() + "\n";
+	std::cout << Vec4.str() + " + " + std::to_string(Num) + " = " + (Vec4 + Num).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorMinusWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Minus With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " - " + std::to_string(Num) + " = " + (Vec2 - Num).str() + "\n";
+	std::cout << Vec3.str() + " - " + std::to_string(Num) + " = " + (Vec3 - Num).str() + "\n";
+	std::cout << Vec4.str() + " - " + std::to_string(Num) + " = " + (Vec4 - Num).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorTimesWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Times With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " * " + std::to_string(Num) + " = " + (Vec2 * Num).str() + "\n";
+	std::cout << Vec3.str() + " * " + std::to_string(Num) + " = " + (Vec3 * Num).str() + "\n";
+	std::cout << Vec4.str() + " * " + std::to_string(Num) + " = " + (Vec4 * Num).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorDevideWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Devide With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " / " + std::to_string(Num) + " = " + (Vec2 / Num).str() + "\n";
+	std::cout << Vec3.str() + " / " + std::to_string(Num) + " = " + (Vec3 / Num).str() + "\n";
+	std::cout << Vec4.str() + " / " + std::to_string(Num) + " = " + (Vec4 / Num).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorModuloWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Modulo With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " % " + std::to_string(Num) + " = " + (Vec2 % Num).str() + "\n";
+	std::cout << Vec3.str() + " % " + std::to_string(Num) + " = " + (Vec3 % Num).str() + "\n";
+	std::cout << Vec4.str() + " % " + std::to_string(Num) + " = " + (Vec4 % Num).str() + "\n";
+}
+
+void WorkingTest::ShowOperatorAddEquelWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Add Equel With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " += " + std::to_string(Num) + " = ";
+	Vec2 += Num;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() + " += " + std::to_string(Num) + " = ";
+	Vec3 += Num;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() + " += " + std::to_string(Num) + " = ";
+	Vec4 += Num;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorMinusEquelWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Minus Equel With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " -= " + std::to_string(Num) + " = ";
+	Vec2 -= Num;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() + " -= " + std::to_string(Num) + " = ";
+	Vec3 -= Num;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() + " -= " + std::to_string(Num) + " = ";
+	Vec4 -= Num;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorTimesEquelWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Times Equel With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " *= " + std::to_string(Num) + " = ";
+	Vec2 *= Num;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() + " *= " + std::to_string(Num) + " = ";
+	Vec3 *= Num;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() + " *= " + std::to_string(Num) + " = ";
+	Vec4 *= Num;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorDevideEquelWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Devide Equel With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " /= " + std::to_string(Num) + " = ";
+	Vec2 /= Num;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() + " /= " + std::to_string(Num) + " = ";
+	Vec3 /= Num;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() + " /= " + std::to_string(Num) + " = ";
+	Vec4 /= Num;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowOperatorModuloEquelWithAnotherNumber()
+{
+	std::cout << "\n----------------------------- Operator Modulo Equel With Another Number ----------------------------\n";
+	std::cout << Vec2.str() + " %= " + std::to_string(Num) + " = ";
+	Vec2 %= Num;
+	std::cout << Vec2.str() + "\n";
+
+	std::cout << Vec3.str() + " %= " + std::to_string(Num) + " = ";
+	Vec3 %= Num;
+	std::cout << Vec3.str() + "\n";
+
+	std::cout << Vec4.str() + " %= " + std::to_string(Num) + " = ";
+	Vec4 %= Num;
+	std::cout << Vec4.str() + "\n";
+}
+
+void WorkingTest::ShowCompare()
+{
+	std::cout << "\n----------------------------- Compare ----------------------------\n";
+	std::cout << Vec2.str() + " == " + Vec2B.str() + " = " + std::to_string(Vec2 == Vec2B) + "\n";
+	std::cout << Vec2.str() + " != " + Vec2B.str() + " = " + std::to_string(Vec2 != Vec2B) + "\n";
+	std::cout << Vec2.Distance() << " > " << Vec2B.Distance() << " = " + std::to_string(Vec2 > Vec2B) + "\n";
+	std::cout << Vec2.Distance() << " >= " << Vec2B.Distance() << " = " + std::to_string(Vec2 >= Vec2B) + "\n";
+	std::cout << Vec2.Distance() << " < " << Vec2B.Distance() << " = " + std::to_string(Vec2 < Vec2B) + "\n";
+	std::cout << Vec2.Distance() << " <= " << Vec2B.Distance() << " = " + std::to_string(Vec2 <= Vec2B) + "\n";
+
+	std::cout << Vec3.str() + " == " + Vec3B.str() + " = " + std::to_string(Vec3 == Vec3B) + "\n";
+	std::cout << Vec3.str() + " != " + Vec3B.str() + " = " + std::to_string(Vec3 != Vec3B) + "\n";
+	std::cout << Vec3.Distance() << " > " << Vec2B.Distance() << " = " + std::to_string(Vec3 > Vec3B) + "\n";
+	std::cout << Vec3.Distance() << " >= " << Vec2B.Distance() << " = " + std::to_string(Vec3 >= Vec3B) + "\n";
+	std::cout << Vec3.Distance() << " < " << Vec2B.Distance() << " = " + std::to_string(Vec3 < Vec3B) + "\n";
+	std::cout << Vec3.Distance() << " <= " << Vec2B.Distance() << " = " + std::to_string(Vec3 <= Vec3B) + "\n";
+
+	std::cout << Vec4.str() + " == " + Vec4B.str() + " = " + std::to_string(Vec4 == Vec4B) + "\n";
+	std::cout << Vec4.str() + " != " + Vec4B.str() + " = " + std::to_string(Vec4 != Vec4B) + "\n";
+	std::cout << Vec4.Distance() << " > " << Vec4B.Distance() << " = " + std::to_string(Vec4 > Vec4B) + "\n";
+	std::cout << Vec4.Distance() << " >= " << Vec4B.Distance() << " = " + std::to_string(Vec4 >= Vec4B) + "\n";
+	std::cout << Vec4.Distance() << " < " << Vec4B.Distance() << " = " + std::to_string(Vec4 < Vec4B) + "\n";
+	std::cout << Vec4.Distance() << " <= " << Vec4B.Distance() << " = " + std::to_string(Vec4 <= Vec4B) + "\n";
 }
